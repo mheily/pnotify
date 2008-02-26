97c83fabba99a9e4087782d5784a6e946ca86a92
mheily <mheily@50d3c15f-2236-0410-b7b9-5510e3558942>
2008-02-26 03:42:43 +0000

    remove vnode tests

--- trunk/test.c
+++ trunk/test.c
@@ -24,7 +24,6 @@ event_cmp(struct event *ev, struct watch *watch, int mask)
 		printf(" *ERROR * mismatch: expecting (watch:mask) of '%p:%d' but got '%p:%d'\n",
 			  watch, mask,
 			  ev->watch, ev->mask);
-		pnotify_print_event(ev);
 	}
 	return i;
 }
@@ -94,58 +93,6 @@ test_dispatch()
 	test (event_dispatch());
 }
 
-static void
-test_vnode()
-{
- 	struct watch *w;
-	struct event    evt;
-
-	/* XXX-FIXME doesnt work with directories right now */
-	return;
-
-	/* Watch for events in the test directory */
-	test((w = watch_vnode(".check", PN_CREATE | PN_DELETE | PN_MODIFY, NULL, NULL))); 
-
-	/* Create a new file */
-	test (system("touch .check/foo"));
-
-	/* Read the event */
-	test (event_wait(&evt)); 
-	if (!event_cmp(&evt, w, PN_MODIFY)) 
-		err(1, "unexpected event value");
-
-	/* Create a new file #2 */
-	test (system("touch .check/bar"));
-
-	/* Read the event */
-	test (event_wait(&evt));
-	if (!event_cmp(&evt, w, PN_MODIFY)) 
-		err(1, "unexpected event value");
-
-	/* Delete the new file */
-	test (system("rm .check/foo"));
-
-	/* Read the delete event */
-	test (event_wait(&evt));
-	if (!event_cmp(&evt, w, PN_MODIFY)) 
-		err(1, "unexpected event value");
-
-#if FIXME
-	/* XXX - BROKEN, does not result in an event */
-	/* It appears FreeBSD doesnt modify the mtime of a directory when the mtime of a file changes,
-	   only when a new file is created. */
-	/* Modify file #2 */
-	test (system("echo hi >> .check/bar"));
-
-	/* Read the modify event */
-	test (event_wait(&evt, ctx));
-	if (!event_cmp(&evt, w, PN_MODIFY)) 
-		err(1, "unexpected event value");
-#endif
-
-	/* Remove the watch */
-	test (pnotify_rm_watch(w));
-}
 
 int
 main(int argc, char **argv)
@@ -160,7 +107,6 @@ main(int argc, char **argv)
 	/* Initialize the queue */
 	test(ctx = pnotify_init());
 
-	test_vnode();
 	test_fd();
 	test_signals();
 	test_timer();
