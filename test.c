4727282c714ec7e92e775408a6d233b6111698f7
mheily <mheily@50d3c15f-2236-0410-b7b9-5510e3558942>
2009-02-21 01:00:22 +0000

    initial import, passes all regression tests

--- test.c
+++ test.c
@@ -1,96 +1,103 @@
+/*              $Id: $          */
+
+/*
+ * Copyright (c) 2009 Mark Heily <mark@heily.com>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <err.h>
-#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
-#include "pnotify.h"
-#include "pnotify-internal.h"
+#include "epoll.h"
 
-int FD_RESULT = -1;
-int TIMER_RESULT = -1;
-int SIGNAL_RESULT = -1;
-
-#define test(x) do { \
-   printf(" * " #x ": "); 				\
-   fflush(stdout);					\
-   printf("%s\n", ((x) >= 0) ? "passed" : "failed");	\
-} while (0)
-
+int epfd;
 
 void
-signal_cb(int signum, void *arg)
+success(const char *func)
 {
-	SIGNAL_RESULT = (signum == SIGUSR1) ? 0 : 1;
-}
-
-static void
-test_signals()
-{
- 	struct watch *w;
-
-	test ((w = watch_signal(SIGUSR1, signal_cb, NULL)));
-	test (kill(getpid(), SIGUSR1));
+    printf("%-70s %s\n", func, "passed");
 }
 
 void
-fd_cb(int fd, int evt, void *arg)
+test_epoll_create(void)
 {
-	FD_RESULT = (evt & PN_READ) ? 0 : 1;
-}
-
-static void
-test_fd()
-{
- 	struct watch *w;
-	int fildes[2];
-
-	printf("fd tests\n");
-	test (pipe(fildes));
-	test ((w = watch_fd(fildes[0], fd_cb, NULL)));
-	if (write(fildes[1], "a", 1) != 1)
-		err(1, "write(2)");
+    if ((epfd = epoll_create(100)) < 0)
+ 	err(1, "epoll_create()");
+    success("epoll_create()");
 }
 
 void
-timer_cb(void *arg)
+test_epoll_ctl_add(void)
 {
-	TIMER_RESULT = 0;
+    struct epoll_event ev;
+
+    ev.events = EPOLLIN;
+    ev.data.fd = STDIN_FILENO;
+
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, STDIN_FILENO, &ev) < 0)
+ 	err(1, "epoll_ctl(EPOLL_CTL_ADD)");
+
+    success("epoll_ctl(EPOLL_CTL_ADD)");
 }
 
-static void
-test_timer()
+void
+test_epoll_wait(void)
 {
- 	struct watch *w;
+    int nfds;
+    struct epoll_event ev;
 
-	test ((w = watch_timer(1, timer_cb, NULL)));
+    /* Infinite wait */
+    puts("press <return> to trigger an event (will wait forever)");
+    nfds = epoll_wait(epfd, &ev, 1, -1);
+    if (nfds < 1)
+ 	err(1, "epoll_wait()");
+    (void) getc(stdin);
+
+    /* Timed wait */
+    puts("press <return> within three seconds to trigger an event");
+    nfds = epoll_wait(epfd, &ev, 1, 3000);
+    if (nfds == 0)
+ 	errx(1, "epoll_wait() timed out");
+    if (nfds < 0)
+ 	err(1, "epoll_wait()");
+
+    success("epoll_wait()");
 }
 
+void
+test_epoll_ctl_del(void)
+{
+    struct epoll_event ev;
 
-int
+    ev.events = EPOLLIN;
+    ev.data.fd = STDIN_FILENO;
+
+    if (epoll_ctl(epfd, EPOLL_CTL_DEL, STDIN_FILENO, &ev) < 0)
+ 	err(1, "epoll_ctl(EPOLL_CTL_DEL)");
+
+    success("epoll_ctl(EPOLL_CTL_DEL)");
+}
+int 
 main(int argc, char **argv)
 {
-	/* Create a test directory */
-	(void) system("rm -rf .check");
-	if (system("mkdir .check") < 0)
-		err(1, "mkdir failed");
-	if (system("mkdir .check/dir") < 0)
-		err(1, "mkdir failed");
+    test_epoll_create();
+    test_epoll_ctl_add();
+    test_epoll_wait();
+    test_epoll_ctl_del();
 
-	pnotify_init();
-
-	test_fd();
-	test_signals();
-	test_timer();
-	sleep(5);	/*XXX-FIXME*/
-	printf ("fd: %d\n", FD_RESULT);
-	printf ("timer: %d\n", TIMER_RESULT);
-	printf ("signal: %d\n", SIGNAL_RESULT);
-
-	if ( FD_RESULT || TIMER_RESULT || SIGNAL_RESULT ) 
-		errx(1, "one or more test(s) failed");
-	exit(0);
+    puts("all tests completed.");
+    return (0);
 }
